aad01514bf25f7a9624adf4ab96e2b9d1b35a419
gabriel <53623479+gabok2@users.noreply.github.com>
2020-01-03 13:22:39 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'import-helpers',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      { // example configuration
+          newlinesBetween: 'always',
+          groups: [
+              'module',
+              '/^@shared/',
+              ['parent', 'sibling', 'index'],
+          ],
+          alphabetize: { order: 'asc', ignoreCase: true },
+      },
+  ],
+  'react-hooks/rules-of-hooks': 'error',
+  'react-hooks/exhaustive-deps': 'warn'
+  },
+};
---
+++ src/pages/Main/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+
+export default function Main() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  async function handleAddUser() {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+    Keyboard.dismiss();
+  }
+
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={setNewUser(newUser)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
+        />
+        <SubmitButton onPress={handleAddUser}>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>Ver Perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
